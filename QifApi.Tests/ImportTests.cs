93452759649697a34558cf07b9f0808848d3517f
David Alpert <dalpert@korewireless.com>
2016-06-26 23:07:55 -0500

    proves can import .qif with data from multiple accounts

--- QifApi.Tests/ImportTests.cs
+++ QifApi.Tests/ImportTests.cs
@@ -12,6 +12,24 @@ namespace QifApi.Tests
     public class ImportTests
     {
         [Test]
+        public void Import_with_multiple_accounts()
+        {
+            var sample = ResourceHelpers.ExtractResourceToString("QifApi.Tests.SampleData.sample.with.one.account.qif");
+
+            using (new SpoofCulture(new CultureInfo("en-US")))
+            using (var reader = new StreamReader(sample.ToUTF8MemoryStream()))
+            {
+                var dom = new QifDom();
+
+                dom.Import(reader);
+
+                Assert.AreEqual(2, dom.AccountListTransactions.Count);
+                Assert.AreEqual("Checking 0101", dom.AccountListTransactions[0].Name);
+                Assert.AreEqual("Savings 0202", dom.AccountListTransactions[1].Name);
+            }
+        }
+
+        [Test]
         public void Can_import_sample_qif_when_current_culture_is_en_US()
         {
             var sample = ResourceHelpers.ExtractResourceToString("QifApi.Tests.SampleData.sample.qif");
